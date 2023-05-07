2417c4d8c47de9447b782171cc12813c0fdaff78
Timothée Vialatoux <ext-timothee@photoroom.com>
2023-05-07 20:07:38 +0200

    Basic layout

--- packages/gala/pages/_app.tsx
+++ packages/gala/pages/_app.tsx
@@ -1,15 +1,16 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import Layout from '../components/Layout';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to gala!</title>
+        <title>GALA de l&apos;Arbresle</title>
       </Head>
-      <main>
+      <Layout>
         <Component {...pageProps} />
-      </main>
+      </Layout>
     </>
   );
 }
