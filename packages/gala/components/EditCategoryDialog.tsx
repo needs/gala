ef3a86ebbad340509f058db87e6a91d5ea3b336c
Timothée Vialatoux <ext-timothee@photoroom.com>
2023-05-08 12:16:04 +0200

    Better gender selector for EditCategoryDialog

--- packages/gala/components/EditCategoryDialog.tsx
+++ packages/gala/components/EditCategoryDialog.tsx
@@ -7,6 +7,8 @@ import {
   DialogActions,
   Button,
   Select,
+  FormLabel,
+  Stack,
 } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { Category } from '../lib/database';
@@ -36,26 +38,36 @@ export default function EditCategoryDialog({
         <DialogContentText>
           Les modifications seront appliquées seulement une fois validées.
         </DialogContentText>
-        <TextField
-          autoFocus
-          margin="dense"
-          label="Nom"
-          type="text"
-          fullWidth
-          variant="standard"
-          value={editedCategory.name}
-          onChange={(event) =>
-            setEditedCategory({ ...editedCategory, name: event.target.value })
-          }
-        />
-        <GenderSelector
-          gender={editedCategory.gender}
-          onChange={(gender) =>
-            setEditedCategory({ ...editedCategory, gender })
-          }
-        />
+        <Stack direction="column" spacing={2}>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Nom"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={editedCategory.name}
+            onChange={(event) =>
+              setEditedCategory({ ...editedCategory, name: event.target.value })
+            }
+          />
+          <Stack
+            direction="row"
+            spacing={2}
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <FormLabel>Genre</FormLabel>
+            <GenderSelector
+              gender={editedCategory.gender}
+              onChange={(gender) =>
+                setEditedCategory({ ...editedCategory, gender })
+              }
+            />
+          </Stack>
 
-        <Select>{/* TODO: Category apparatuses */}</Select>
+          <Select>{/* TODO: Category apparatuses */}</Select>
+        </Stack>
       </DialogContent>
       <DialogActions>
         <Button onClick={onCancel}>Annuler</Button>
