6a6f53354dcd9c9adb476b9fe3bc1f9614448d49
Timothée Vialatoux <ext-timothee@photoroom.com>
2023-05-13 17:28:43 +0200

    Vercel analytics

--- packages/gala/pages/_app.tsx
+++ packages/gala/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 import '../styles/global.css';
+import { Analytics } from '@vercel/analytics/react';
+
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
