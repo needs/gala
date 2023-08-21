bac089a769e2959a87166379832685c47797c4e6
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2023-08-21 12:24:26 +0200

    Introduce Firebase authentication

--- packages/gala/pages/index.tsx
+++ packages/gala/pages/index.tsx
@@ -1,6 +1,27 @@
-import { Box, Button, CssBaseline, Stack, TextField, Typography } from '@mui/material';
+import { Add, EmojiEvents } from '@mui/icons-material';
+import {
+  Avatar,
+  Box,
+  Button,
+  CssBaseline,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText,
+  Stack,
+  Typography,
+} from '@mui/material';
+import { trpc } from '../utils/trpc';
+import { useRouter } from 'next/router';
+import { signOut } from 'firebase/auth';
+import { auth } from '../lib/firebase';
 
-export default function Index() {
+export default function ListPage() {
+  const create = trpc.create.useMutation();
+  const { data: galas } = trpc.list.useQuery(null);
+  const router = useRouter();
+
   return (
     <Box
       minWidth="100vw"
@@ -9,30 +30,80 @@ export default function Index() {
       flexDirection="column"
       alignItems="center"
       justifyContent="center"
-      sx={{ backgroundImage: 'url("/background.svg")', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center' }}
+      sx={{
+        backgroundImage: 'url("/background.svg")',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
     >
       <CssBaseline />
 
-      <Stack gap={2} width="600px" padding={4} sx={{ backgroundColor: "white" }} borderRadius="10px">
-        <Typography>Authentifiez vous ou créez un compte pour retrouver vos GALA de gymnastique.</Typography>
-        <TextField required label="Email" variant="outlined" />
-        <TextField
-          required
-          label="Mot de passe"
-          variant="outlined"
-          type="password"
-        />
-        <Stack direction="row" gap={2}>
-          <Button variant="outlined" fullWidth>
-            Inscription
-          </Button>
-          <Button variant="contained" fullWidth>
-            Connexion
+      <Stack
+        gap={2}
+        width="600px"
+        padding={4}
+        sx={{ backgroundColor: 'white' }}
+        borderRadius="10px"
+      >
+        <Stack
+          direction="row"
+          gap={2}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Typography variant="h6" component="h1">
+            Vos GALAs
+          </Typography>
+          <Button
+            variant="contained"
+            startIcon={<Add />}
+            onClick={() => {
+              create.mutateAsync(null).then((gala) => {
+                router.push(`/gala/${gala.uuid}/teams`);
+              });
+            }}
+          >
+            Créer un GALA
           </Button>
         </Stack>
+        <List>
+          {galas !== undefined && galas.length === 0 && (
+            <ListItem disablePadding>
+              <ListItemText
+                primary="Vous n'avez pas encore créé de GALA"
+                secondary="Cliquez sur le bouton ci-dessus pour en créer un"
+              />
+              </ListItem>
+          )}
+          {galas !== undefined && galas.length > 0 &&
+            galas.map((gala) => (
+              <ListItem key={gala.uuid} disablePadding>
+                <ListItemButton onClick={() => router.push(`/gala/${gala.uuid}/teams`)}>
+                  <ListItemAvatar>
+                    <Avatar>
+                      <EmojiEvents />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="GALA de l'Arbresle"
+                    secondary={`${gala.teamCount} équipes`}
+                  />
+                </ListItemButton>
+              </ListItem>
+            ))}
+        </List>
+
+        <Button onClick={() => {
+          signOut(auth).then(() => {
+            router.push('/login');
+          })
+        }} color="secondary">
+          Deconnexion
+        </Button>
       </Stack>
     </Box>
   );
 }
 
-Index.disableStoreProvider = true;
+ListPage.disableStoreProvider = true;
---
+++ packages/gala/pages/login.tsx
@@ -0,0 +1,229 @@
+import {
+  Alert,
+  Box,
+  Button,
+  CssBaseline,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
+import {
+  UserCredential,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  sendEmailVerification,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { auth } from '../lib/firebase';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
+function Success() {
+  const router = useRouter();
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    router.push('/');
+
+    const timeout = setTimeout(() => {
+      setDisabled(false);
+    }, 2000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [router]);
+
+  return (
+    <>
+      <Alert severity="success">
+        {
+          "Redirection vers l'application en cours. Si la redirection ne fonctionne pas, veuillez cliquer sur le bouton ci-dessous."
+        }
+      </Alert>
+      <Button href="/" disabled={disabled}>
+        Redirection
+      </Button>
+    </>
+  );
+}
+
+function VerifyEmail() {
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDisabled(false);
+    }, 10000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
+  return (
+    <>
+      <Alert severity="info">
+        {
+          "Un email de confirmation vous a été envoyé. Veuillez cliquer sur le lien dans l'email pour continuer."
+        }
+      </Alert>
+
+      <Button
+        onClick={() => {
+          if (auth.currentUser !== null) {
+            sendEmailVerification(auth.currentUser);
+          }
+        }}
+        disabled={disabled}
+      >
+        Renvoyer un email de vérification
+      </Button>
+    </>
+  );
+}
+
+function Login() {
+  const router = useRouter();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(
+    undefined
+  );
+
+  const validateUser = (userCredential: UserCredential) => {
+    const user = userCredential.user;
+    if (!user.emailVerified) {
+      sendEmailVerification(user).catch((error) => {
+        setErrorMessage(error.message);
+      });
+    } else {
+      router.push('/');
+    }
+  };
+
+  return (
+    <>
+      <Typography>
+        Authentifiez vous ou créez un compte pour retrouver vos GALA de
+        gymnastique.
+      </Typography>
+      {errorMessage !== undefined && (
+        <Alert severity="error">{errorMessage}</Alert>
+      )}
+      <TextField
+        required
+        label="Email"
+        variant="outlined"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <TextField
+        required
+        label="Mot de passe"
+        variant="outlined"
+        type="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <Stack direction="row" gap={2}>
+        <Button
+          variant="outlined"
+          fullWidth
+          onClick={() => {
+            setErrorMessage(undefined);
+            createUserWithEmailAndPassword(auth, email, password)
+              .then((userCredential) => {
+                validateUser(userCredential);
+              })
+              .catch((error) => {
+                setErrorMessage(error.message);
+              });
+          }}
+        >
+          Inscription
+        </Button>
+        <Button
+          variant="contained"
+          fullWidth
+          onClick={() => {
+            setErrorMessage(undefined);
+            signInWithEmailAndPassword(auth, email, password)
+              .then((userCredential) => {
+                validateUser(userCredential);
+              })
+              .catch((error) => {
+                setErrorMessage(error.message);
+              });
+          }}
+        >
+          Connexion
+        </Button>
+      </Stack>
+    </>
+  );
+}
+
+export default function Index() {
+  const [step, setStep] = useState<'login' | 'verify' | 'success'>('login');
+
+  useEffect(() => {
+    return onAuthStateChanged(auth, (user) => {
+      if (user === null) {
+        setStep('login');
+      } else if (!user.emailVerified) {
+        setStep('verify');
+      } else {
+        setStep('success');
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (step === 'verify') {
+      const interval = setInterval(() => {
+        if (auth.currentUser !== null) {
+          auth.currentUser.reload();
+        }
+      }, 1000);
+
+      return () => {
+        clearInterval(interval);
+      }
+    }
+  }, [step]);
+
+  return (
+    <Box
+      minWidth="100vw"
+      minHeight="100vh"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        backgroundImage: 'url("/background.svg")',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <CssBaseline />
+
+      <Stack
+        gap={2}
+        width="600px"
+        padding={4}
+        sx={{ backgroundColor: 'white' }}
+        borderRadius="10px"
+      >
+        {step === 'login' && <Login />}
+        {step === 'verify' && <VerifyEmail />}
+        {step === 'success' && <Success />}
+      </Stack>
+    </Box>
+  );
+}
+
+Index.disableStoreProvider = true;
