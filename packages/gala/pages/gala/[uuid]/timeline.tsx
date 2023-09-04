43606b0935a9f02080e4eb7f66ab3c212c7ea2ec
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2023-09-04 11:37:49 +0200

    Remove Timeline to cleanup the timeline page

--- packages/gala/pages/gala/[uuid]/timeline.tsx
+++ packages/gala/pages/gala/[uuid]/timeline.tsx
@@ -7,20 +7,11 @@ import {
   Paper,
   Stack,
   TextField,
+  Tooltip,
   Typography,
 } from '@mui/material';
 import { withAuthGala } from '../../../lib/auth';
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
-import {
-  Timeline,
-  TimelineConnector,
-  TimelineContent,
-  TimelineDot,
-  TimelineItem,
-  TimelineOppositeContent,
-  TimelineSeparator,
-  timelineOppositeContentClasses,
-} from '@mui/lab';
 import Image from 'next/image';
 import {
   Add,
@@ -40,11 +31,7 @@ import {
 } from '../../../lib/store';
 import { useSyncedStore } from '@syncedstore/react';
 import { uuidv4 } from 'lib0/random';
-import {
-  addMinutes,
-  format,
-  startOfDay,
-} from 'date-fns';
+import { addMinutes, format, startOfDay } from 'date-fns';
 import EditTeamDialog from '../../../components/EditTeamDialog';
 import { useState } from 'react';
 import { sortBy } from 'lodash';
@@ -199,7 +186,7 @@ export default function TimelinePage() {
                   },
                 },
                 order: 0,
-                durationInMinutes: 10,
+                durationInMinutes: 60,
               };
             }}
             startIcon={<Add />}
@@ -209,246 +196,206 @@ export default function TimelinePage() {
         </Stack>
       </Stack>
 
-      <Timeline
-        sx={{
-          [`& .${timelineOppositeContentClasses.root}`]: {
-            flex: 0,
-          },
-        }}
-      >
-        <TimelineItem>
-          <TimelineOppositeContent
-            color="textSecondary"
-            minWidth="80px"
-          ></TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent sx={{ paddingTop: '0' }}>
-            <Stack direction="column" gap={2} paddingBottom={4}>
-              <Typography variant="h6" component="h1">
-                Ouverture du gymnase
-              </Typography>
-              <DateTimePicker
-                label="Date"
-                sx={{
-                  minWidth: '400px',
-                }}
-                value={new Date(stage.timelineStartDate)}
-                onChange={(date) => {
-                  if (date !== null) {
-                    stage.timelineStartDate = date.toString();
-                  }
-                }}
-                format="EEEE d MMMM yyyy HH:mm"
-              />
-            </Stack>
-          </TimelineContent>
-        </TimelineItem>
+      <Stack direction="column" gap={2}>
+        <Typography variant="h6" component="h1">
+          Ouverture du gymnase
+        </Typography>
+        <DateTimePicker
+          label="Date"
+          sx={{
+            minWidth: '400px',
+          }}
+          value={new Date(stage.timelineStartDate)}
+          onChange={(date) => {
+            if (date !== null) {
+              stage.timelineStartDate = date.toString();
+            }
+          }}
+          format="EEEE d MMMM yyyy HH:mm"
+        />
+      </Stack>
 
-        {rotations.map(([rotationKey, rotation], order) => {
-          const rotationDate = nextRotationDate;
+      {rotations.map(([rotationKey, rotation], order) => {
+        const rotationDate = nextRotationDate;
 
-          nextRotationDate = addMinutes(
-            nextRotationDate,
-            rotation.durationInMinutes
-          );
-          const durationDate = addMinutes(
-            startOfDayDate,
-            rotation.durationInMinutes
-          );
+        nextRotationDate = addMinutes(
+          nextRotationDate,
+          rotation.durationInMinutes
+        );
 
-          return (
-            rotation !== undefined && (
-              <TimelineItem key={rotationKey}>
-                <TimelineOppositeContent color="textSecondary" minWidth="80px">
-                  <Stack gap={2} alignItems="end">
-                    <Typography variant="body1">
-                      {format(rotationDate, 'HH:mm')}
-                    </Typography>
-                    <IconButton
-                      size="small"
-                      disabled={order === 0}
-                      onClick={() => {
-                        rotations[order][1].order -= 1;
-                        rotations[order - 1][1].order += 1;
-                      }}
-                    >
-                      <ArrowUpward />
-                    </IconButton>
-                    <IconButton
-                      size="small"
-                      disabled={
-                        order === Object.keys(stage.timeline).length - 1
-                      }
-                      onClick={() => {
-                        rotations[order][1].order += 1;
-                        rotations[order + 1][1].order -= 1;
-                      }}
-                    >
-                      <ArrowDownward />
-                    </IconButton>
-                  </Stack>
-                </TimelineOppositeContent>
-                <TimelineSeparator>
-                  <TimelineDot />
-                  <TimelineConnector />
-                </TimelineSeparator>
-                <TimelineContent marginBottom={4} sx={{ paddingTop: '0' }}>
-                  <Stack gap={2}>
-                    <Stack
-                      direction="row"
-                      gap={2}
-                      alignItems="center"
-                      justifyContent="space-between"
-                    >
-                      <Stack
-                        direction="row"
-                        gap={2}
-                        alignItems="center"
-                        divider={<Divider />}
-                      >
-                        <Typography variant="h6" component="h1">
-                          Rotation {order + 1}
-                        </Typography>
-                        <TextField
-                          label="Durée"
-                          inputProps={{
-                            inputMode: 'numeric',
-                            pattern: '[0-9]*',
-                          }}
-                          InputProps={{
-                            endAdornment: (
-                              <InputAdornment position="end">
-                                minutes
-                              </InputAdornment>
-                            ),
-                          }}
-                          size="small"
-                          value={rotation.durationInMinutes}
-                          onChange={(event) => {
-                            const duration = parseInt(event.target.value);
-                            rotation.durationInMinutes = isNaN(duration)
-                              ? 0
-                              : duration;
-                          }}
-                          sx={{
-                            maxWidth: '130px',
-                          }}
-                        />
-                      </Stack>
+        const isEmpty = Object.values(rotation.apparatuses).every(
+          (apparatus) => Object.keys(apparatus.teams).length === 0
+        );
+
+        return (
+          rotation !== undefined && (
+            <Stack gap={2}>
+              <Stack
+                direction="row"
+                gap={2}
+                alignItems="center"
+                justifyContent="space-between"
+              >
+                <Stack
+                  direction="row"
+                  gap={2}
+                  alignItems="center"
+                  divider={<Divider />}
+                >
+                  <Typography variant="h6" component="h1">
+                    {format(rotationDate, 'HH:mm')}
+                  </Typography>
+                  <TextField
+                    label="Durée"
+                    inputProps={{
+                      inputMode: 'numeric',
+                      pattern: '[0-9]*',
+                    }}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">minutes</InputAdornment>
+                      ),
+                    }}
+                    size="small"
+                    value={rotation.durationInMinutes}
+                    onChange={(event) => {
+                      const duration = parseInt(event.target.value);
+                      rotation.durationInMinutes = isNaN(duration)
+                        ? 0
+                        : duration;
+                    }}
+                    sx={{
+                      maxWidth: '130px',
+                    }}
+                  />
+                </Stack>
+                <Stack gap={2} direction="row">
+                  <IconButton
+                    size="small"
+                    disabled={order === 0}
+                    onClick={() => {
+                      rotations[order][1].order -= 1;
+                      rotations[order - 1][1].order += 1;
+                    }}
+                  >
+                    <ArrowUpward />
+                  </IconButton>
+                  <IconButton
+                    size="small"
+                    disabled={order === Object.keys(stage.timeline).length - 1}
+                    onClick={() => {
+                      rotations[order][1].order += 1;
+                      rotations[order + 1][1].order -= 1;
+                    }}
+                  >
+                    <ArrowDownward />
+                  </IconButton>
+
+                  <Tooltip
+                    title={
+                      isEmpty
+                        ? undefined
+                        : 'Cette rotation doit être vide pour pouvoir être enlevée'
+                    }
+                  >
+                    <span>
                       <IconButton
                         size="small"
                         onClick={() => {
                           delete stage.timeline[rotationKey];
                         }}
+                        disabled={!isEmpty}
                       >
                         <Delete />
                       </IconButton>
-                    </Stack>
-                    <Paper elevation={1} sx={{ overflow: 'hidden' }}>
-                      <Grid container>
-                        {Object.entries(rotation.apparatuses).map(
-                          ([apparatuseKey, { teams: apparatusTeams }]) => (
-                            <Grid
-                              key={apparatuseKey}
-                              xs
-                              sx={{
-                                '&:nth-of-type(odd)': {
-                                  backgroundColor: (theme) =>
-                                    theme.palette.grey[50],
-                                },
-                              }}
+                    </span>
+                  </Tooltip>
+                </Stack>
+              </Stack>
+              <Paper elevation={1} sx={{ overflow: 'hidden' }}>
+                <Grid container>
+                  {Object.entries(rotation.apparatuses).map(
+                    ([apparatuseKey, { teams: apparatusTeams }]) => (
+                      <Grid
+                        key={apparatuseKey}
+                        xs
+                        sx={{
+                          '&:nth-of-type(odd)': {
+                            backgroundColor: (theme) => theme.palette.grey[50],
+                          },
+                        }}
+                      >
+                        <Stack alignItems="stretch" flexGrow={1}>
+                          <Stack
+                            direction="row"
+                            gap={2}
+                            justifyContent="space-between"
+                            padding={2}
+                            sx={{
+                              backgroundColor: '#a5a5a511',
+                            }}
+                          >
+                            <Stack
+                              direction="row"
+                              gap={2}
+                              alignItems="center"
+                              justifyContent="center"
                             >
-                              <Stack alignItems="stretch" flexGrow={1}>
-                                <Stack
-                                  direction="row"
-                                  gap={2}
-                                  justifyContent="space-between"
-                                  padding={2}
-                                  sx={{
-                                    backgroundColor: '#a5a5a511',
-                                  }}
-                                >
-                                  <Stack
-                                    direction="row"
-                                    gap={2}
-                                    alignItems="center"
-                                    justifyContent="center"
-                                  >
-                                    <Image
-                                      src={getApparatusIconPath(
-                                        apparatuseKey as ApparatusKey
-                                      )}
-                                      alt={getApparatusName(
-                                        apparatuseKey as ApparatusKey
-                                      )}
-                                      width={24}
-                                      height={24}
-                                    />
-                                    <Typography variant="h6">
-                                      {getApparatusName(
-                                        apparatuseKey as ApparatusKey
-                                      )}
-                                    </Typography>
-                                  </Stack>
+                              <Image
+                                src={getApparatusIconPath(
+                                  apparatuseKey as ApparatusKey
+                                )}
+                                alt={getApparatusName(
+                                  apparatuseKey as ApparatusKey
+                                )}
+                                width={24}
+                                height={24}
+                              />
+                              <Typography variant="h6">
+                                {getApparatusName(
+                                  apparatuseKey as ApparatusKey
+                                )}
+                              </Typography>
+                            </Stack>
 
-                                  <TimelineAddTeamButton
-                                    teamsMap={apparatusTeams}
+                            <TimelineAddTeamButton teamsMap={apparatusTeams} />
+                          </Stack>
+
+                          <Stack
+                            flexGrow={1}
+                            direction="column"
+                            divider={<Divider />}
+                          >
+                            {Object.keys(apparatusTeams).map((teamKey) => {
+                              const team = teams[teamKey];
+
+                              return (
+                                team !== undefined && (
+                                  <TimelineEditTeamButton
+                                    key={teamKey}
+                                    team={team}
+                                    onRemove={() => {
+                                      delete apparatusTeams[teamKey];
+                                    }}
                                   />
-                                </Stack>
-
-                                <Stack
-                                  flexGrow={1}
-                                  direction="column"
-                                  divider={<Divider />}
-                                >
-                                  {Object.keys(apparatusTeams).map(
-                                    (teamKey) => {
-                                      const team = teams[teamKey];
-
-                                      return (
-                                        team !== undefined && (
-                                          <TimelineEditTeamButton
-                                            key={teamKey}
-                                            team={team}
-                                            onRemove={() => {
-                                              delete apparatusTeams[teamKey];
-                                            }}
-                                          />
-                                        )
-                                      );
-                                    }
-                                  )}
-                                </Stack>
-                              </Stack>
-                            </Grid>
-                          )
-                        )}
+                                )
+                              );
+                            })}
+                          </Stack>
+                        </Stack>
                       </Grid>
-                    </Paper>
-                  </Stack>
-                </TimelineContent>
-              </TimelineItem>
-            )
-          );
-        })}
+                    )
+                  )}
+                </Grid>
+              </Paper>
+            </Stack>
+          )
+        );
+      })}
 
-        <TimelineItem>
-          <TimelineOppositeContent color="textSecondary" minWidth="80px">
-            {format(nextRotationDate, 'HH:mm')}
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-          </TimelineSeparator>
-          <TimelineContent sx={{ paddingTop: '0' }}>
-            <Typography variant="h6" component="h1">
-              Fin de la compétition
-            </Typography>
-          </TimelineContent>
-        </TimelineItem>
-      </Timeline>
+      <Typography variant="h6" component="h1">
+        {format(nextRotationDate, 'HH:mm')} - Fin de la compétition
+      </Typography>
     </Stack>
   );
 }
