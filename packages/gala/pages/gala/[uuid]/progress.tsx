8e85f1e6fbd1149d19dc669df8c916bd932d1727
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2023-09-23 17:23:29 +0200

    Progress screen

--- packages/gala/pages/gala/[uuid]/progress.tsx
+++ packages/gala/pages/gala/[uuid]/progress.tsx
@@ -1,12 +1,8 @@
 import { Alert, Button, Stack, Typography } from '@mui/material';
 import {
   ApparatusKey,
-  Stage,
-  TimelinePause,
   TimelineRotation,
-  TimelineRotationApparatus,
   stageApparatuses,
-  stageRotations,
   useGala,
 } from '../../../lib/store';
 import { withAuthGala } from '../../../lib/auth';
@@ -15,13 +11,13 @@ import TimelinePause_ from '../../../components/TimelinePause';
 import { ArrowBack, ArrowForward } from '@mui/icons-material';
 import { ReactNode } from 'react';
 import {
-  addMinutes,
   format,
   formatDuration,
   intervalToDuration,
 } from 'date-fns';
 import fr from 'date-fns/locale/fr';
 import { isEmpty } from 'lodash';
+import { ProgressGenericInfo, getCurrentRotation } from '../../../lib/progress';
 
 function formatRotationTime(startDate?: Date, endDate?: Date) {
   if (startDate === undefined && endDate === undefined) {
@@ -241,133 +237,6 @@ function ProgressPause({
       <TimelinePause_ />
     </ProgressContainer>
   );
-}
-
-type CurrentRotation =
-  | {
-      type: 'start';
-    }
-  | {
-      type: 'end';
-    }
-  | {
-      type: 'rotation';
-      rotation: TimelineRotation;
-    }
-  | {
-      type: 'pause';
-      rotation: TimelinePause;
-    };
-
-type ProgressGenericInfo = {
-  index: number;
-  count: number;
-};
-
-type CurrentRotationInfo = {
-  startDate: Date | undefined;
-  endDate: Date | undefined;
-  currentRotation: CurrentRotation;
-
-  timeslotInfo?: ProgressGenericInfo;
-  rotationInfo?: ProgressGenericInfo;
-};
-
-function mod(n: number, m: number) {
-  'use strict';
-  return ((n % m) + m) % m;
-}
-
-function getCurrentRotation(stage: Stage): CurrentRotationInfo {
-  const progress = stage.progress;
-  const rotations = stageRotations(stage);
-  const apparatuses = stageApparatuses(stage);
-
-  let tmpProgress = 0;
-  let startDate = new Date(stage.timelineStartDate);
-
-  if (progress === undefined || progress < 0) {
-    return {
-      startDate,
-      endDate: undefined,
-      currentRotation: {
-        type: 'start',
-      },
-    };
-  }
-
-  let timeslotIndex = 1;
-
-  for (const rotation of rotations) {
-    const endDate = addMinutes(startDate, rotation.durationInMinutes);
-
-    if (rotation.type === 'pause') {
-      tmpProgress += 1;
-
-      if (progress < tmpProgress) {
-        return {
-          startDate,
-          endDate,
-          currentRotation: {
-            type: 'pause',
-            rotation,
-          },
-
-          timeslotInfo: {
-            index: timeslotIndex,
-            count: rotations.length,
-          },
-        };
-      }
-    } else if (rotation.type === 'rotation') {
-      tmpProgress += apparatuses.length;
-      console.log(JSON.parse(JSON.stringify(rotation)));
-
-      if (progress < tmpProgress) {
-        const offset = progress - tmpProgress + apparatuses.length;
-
-        return {
-          startDate,
-          endDate,
-          currentRotation: {
-            type: 'rotation',
-            rotation: {
-              type: 'rotation',
-              apparatuses: Object.fromEntries(
-                apparatuses.map((apparatusKey, index) => [
-                  apparatusKey,
-                  rotation.apparatuses[
-                    apparatuses[mod(index - offset, apparatuses.length)]
-                  ],
-                ])
-              ) as Record<ApparatusKey, TimelineRotationApparatus>,
-              order: rotation.order,
-              durationInMinutes: rotation.durationInMinutes,
-            },
-          },
-          timeslotInfo: {
-            index: timeslotIndex,
-            count: rotations.length,
-          },
-          rotationInfo: {
-            index: offset + 1,
-            count: apparatuses.length,
-          },
-        };
-      }
-    }
-
-    startDate = endDate;
-    timeslotIndex += 1;
-  }
-
-  return {
-    startDate: undefined,
-    endDate: startDate,
-    currentRotation: {
-      type: 'end',
-    },
-  };
 }
 
 export default function ProgressPage() {
