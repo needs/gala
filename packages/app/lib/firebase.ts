1812d2538014058f0c55aabb7b135a21de1dfe12
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2023-10-07 21:44:58 +0200

    Consolidate firebase config values into a single JSON

--- packages/app/lib/firebase.ts
+++ packages/app/lib/firebase.ts
@@ -1,22 +1,48 @@
 import { FirebaseApp, initializeApp } from 'firebase/app';
 import { Auth, getAuth } from 'firebase/auth';
+import { z } from 'zod';
 
 const globalForFirebase = globalThis as unknown as {
   app: FirebaseApp | undefined;
   auth: Auth | undefined;
 };
 
+const firebaseConfigSchema = z.object({
+  apiKey: z.string(),
+  authDomain: z.string(),
+  projectId: z.string(),
+  storageBucket: z.string(),
+  messagingSenderId: z.string(),
+  appId: z.string(),
+  measurementId: z.string(),
+});
+
+function getFirebaseConfig() {
+  const FIREBASE_CONFIG = process.env["NEXT_PUBLIC_FIREBASE_CONFIG"];
+
+  if (FIREBASE_CONFIG !== undefined) {
+    try {
+      return firebaseConfigSchema.parse(JSON.parse(FIREBASE_CONFIG));
+    } catch (e) {
+      console.error(e);
+      console.warn("Using regular env vars for Firebase config.");
+    }
+  }
+
+  return firebaseConfigSchema.parse({
+    apiKey: process.env["NEXT_PUBLIC_FIREBASE_API_KEY"],
+    authDomain: process.env["NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN"],
+    projectId: process.env["NEXT_PUBLIC_FIREBASE_PROJECT_ID"],
+    storageBucket: process.env["NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET"],
+    messagingSenderId: process.env["NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID"],
+    appId: process.env["NEXT_PUBLIC_FIREBASE_APP_ID"],
+    measurementId: process.env["NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID"],
+  });
+}
+
 export const getFirebaseApp = () => {
   if (globalForFirebase.app === undefined) {
-    const firebaseConfig = {
-      apiKey: process.env["NEXT_PUBLIC_FIREBASE_API_KEY"],
-      authDomain: process.env["NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN"],
-      projectId: process.env["NEXT_PUBLIC_FIREBASE_PROJECT_ID"],
-      storageBucket: process.env["NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET"],
-      messagingSenderId: process.env["NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID"],
-      appId: process.env["NEXT_PUBLIC_FIREBASE_APP_ID"],
-      measurementId: process.env["NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID"],
-    };
+    const firebaseConfig = getFirebaseConfig();
 
     const app = initializeApp(firebaseConfig);
     globalForFirebase.app = app;
--- packages/auth/src/lib/firebase.ts
+++ packages/auth/src/lib/firebase.ts
@@ -1,11 +1,35 @@
 import admin from 'firebase-admin';
+import z from 'zod';
+
+const firebaseConfigSchema = z.object({
+  projectId: z.string(),
+  privateKey: z.string().transform((value) => value.replace(/\\n/g, '\n')),
+  clientEmail: z.string(),
+});
+
+function getFirebaseConfig() {
+  const FIREBASE_CONFIG = process.env["FIREBASE_CONFIG"];
+
+  if (FIREBASE_CONFIG !== undefined) {
+    try {
+      return firebaseConfigSchema.parse(JSON.parse(FIREBASE_CONFIG));
+    } catch (e) {
+      console.error(e);
+      console.warn("Using regular env vars for Firebase config.");
+    }
+  }
+
+  return firebaseConfigSchema.parse({
+    projectId: process.env["FIREBASE_PROJECT_ID"],
+    privateKey: process.env["FIREBASE_PRIVATE_KEY"],
+    clientEmail: process.env["FIREBASE_CLIENT_EMAIL"],
+  });
+}
 
 export const initializeAdminApp = () => {
+  const firebaseConfig = getFirebaseConfig();
+
   return admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env['FIREBASE_PROJECT_ID'],
-      privateKey: process.env['FIREBASE_PRIVATE_KEY']?.replace(/\\n/g, '\n'),
-      clientEmail: process.env['FIREBASE_CLIENT_EMAIL'],
-    }),
+    credential: admin.credential.cert(firebaseConfig),
   }, "admin");
 }
