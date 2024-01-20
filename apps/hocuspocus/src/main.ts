04660421378013d92de750c9a5701535a6220ff1
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2024-01-20 14:10:34 +0100

    Remove more useless logging messages

--- apps/hocuspocus/src/main.ts
+++ apps/hocuspocus/src/main.ts
@@ -33,7 +33,6 @@ const server = new Hocuspocus({
     new Logger(),
     new Database({
       fetch: async ({ documentName }) => {
-        console.log('fetching', documentName)
         const competition = await prisma.competition.findUnique({
           where: {
             uuid: documentName,
@@ -43,7 +42,6 @@ const server = new Hocuspocus({
         if (competition === null) {
           return new Uint8Array();
         }
-        console.log('fetched', documentName)
 
         return Uint8Array.from(competition.data);
       },
