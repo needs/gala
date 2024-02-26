34ddf55e8352f4d6af569d1b2c56dd299ae57e64
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2024-02-26 13:50:27 +0100

    Integrate Google analytics

--- apps/app/pages/_app.tsx
+++ apps/app/pages/_app.tsx
@@ -9,6 +9,7 @@ import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import fr from 'date-fns/locale/fr';
 import '../components/TvFrame.css';
 import { AuthProvider } from '../components/AuthProvider';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export type PageProps = {
   competitionUuid?: string;
@@ -20,12 +21,15 @@ type AppPropsWithProoperties = AppProps & {
   pageProps: PageProps;
 };
 
+const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+
 function CustomApp({ Component, pageProps }: AppPropsWithProoperties) {
   const competitionUuid = pageProps.competitionUuid;
 
   return (
     <AuthProvider>
       <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={fr}>
+        {gaId !== undefined && <GoogleAnalytics gaId={gaId} />}
         {competitionUuid !== undefined ? (
           <StoreProvider competitionUuid={pageProps.competitionUuid}>
             <Layout
